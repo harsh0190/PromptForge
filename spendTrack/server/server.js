fe2d11276a582ea979d13e88d0f2dc21f191436b
Harsh <hsfbd123@gmail.com>
2026-06-29 17:44:00 +0530

    server files

---
+++ spendTrack/server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
