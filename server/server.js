db984315ae21571481e57a79912c22cd2f4c7d47
Harsh Sharma <hsfbd123@gmail.com>
2026-09-02 13:48:05 +0530

    added changes

--- server/server.js
+++ server/server.js
@@ -23,6 +23,6 @@ app.use((err, _req, res, _next) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
